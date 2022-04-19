fadaaa8bbcc619cc45739c4095f0835c3811d623
alimirzayev <alimirzayev0414@gmail.com>
2022-04-19 12:33:54 +0400

    fixed small bugs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,19 @@ import illustration1 from '../assets/img/illustration-1.png'
 import illustration2 from '../assets/img/illustration-2.png'
 import gradient1 from '../assets/img/gradient1.svg'
 import circle from '../assets/img/circle.svg'
-import facebook from '../assets/img/fb.svg'
-import instagram from '../assets/img/insta.svg'
-import tiktok from '../assets/img/tiktok.svg'
-import twitter from '../assets/img/twitter.svg'
-import telegram from '../assets/img/tg.svg'
-import youtube from '../assets/img/youtube.svg'
+import facebook from '../assets/img/fb.png'
+import instagram from '../assets/img/ig.png'
+import tiktok from '../assets/img/tiktok.png'
+import twitter from '../assets/img/twitter.png'
+import telegram from '../assets/img/tg.png'
+import youtube from '../assets/img/ytb.png'
 import bg1 from '../assets/img/Titik-titik.png'
 import shadow from '../assets/img/lightshadow.png'
 
 
 function Main() {
   return (
-    <div className='wrapper'>
+    <div className='wrapper wrapper-main'>
         <main className='main-wrapper'>
 
             <div className="illustration1">
@@ -50,12 +50,12 @@ function Main() {
                 <img className="social-circle-bg-img" src={bg1} alt="bg1" />
                 
                 <div className="social-circle-icons">
-                    <img className='social-circle-facebook-img' src={facebook} alt="facebook" />
-                    <img className='social-circle-youtube-img' src={youtube} alt="youtube" />
-                    <img className='social-circle-instagram-img' src={instagram} alt="instagram" />
-                    <img className='social-circle-telegram-img' src={telegram} alt="telegram" />
-                    <img className='social-circle-twitter-img' src={twitter} alt="twitter" />
-                    <img className='social-circle-tiktok-img' src={tiktok} alt="tiktok" />
+                    <img className='social-circle-facebook-img social-circle-icon' src={facebook} alt="facebook" />
+                    <img className='social-circle-youtube-img social-circle-icon' src={youtube} alt="youtube" />
+                    <img className='social-circle-instagram-img social-circle-icon' src={instagram} alt="instagram" />
+                    <img className='social-circle-telegram-img social-circle-icon' src={telegram} alt="telegram" />
+                    <img className='social-circle-twitter-img social-circle-icon' src={twitter} alt="twitter" />
+                    <img className='social-circle-tiktok-img social-circle-icon' src={tiktok} alt="tiktok" />
                 </div>
 
             </div>
--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../assets/css/Partner.css'
-import twitter from '../assets/img/twitter.svg'
-import telegram from '../assets/img/tg.svg'
-import youtube from '../assets/img/youtube.svg'
+import twitter from '../assets/img/twitter.png'
+import telegram from '../assets/img/tg.png'
+import youtube from '../assets/img/ytb.png'
 import caesar from '../assets/img/caesar.png'
+import elliot from '../assets/img/elliot.png'
+import travlado from '../assets/img/travlado.png'
+import lightshadow from '../assets/img/lightshadow.png'
 
 function Partner() {
   return (
@@ -13,6 +16,10 @@ function Partner() {
             <h3>TOP PARTNERS 😎</h3>
         </div>
 
+        <div className='partner-first-shadow'>
+            <img src={lightshadow} alt="" />
+        </div>
+
         <div className="partner-container">
 
             <div className="partner-first">
@@ -20,6 +27,17 @@ function Partner() {
                 <div className="partner-first-images">
                     <img className='parnter-first-icon' src={telegram} alt="telegram" />
                     <img className='parnter-first-img' src={caesar} alt="caesar" />
+                    <svg className='partner-first-bg-circle' width="210" height="210" viewBox="0 0 210 210" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="105" cy="105" r="20.8" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="34.6667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="48.5333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="62.4" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="76.2667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="90.1333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="104" stroke="white" stroke-width="2"/>
+                    </svg>
+
+
                 </div>
 
                 <div className="partner-first-desc">
@@ -30,10 +48,50 @@ function Partner() {
             </div>
 
             <div className="partner-second">
-                
+
+                <div className="partner-second-images">
+                    <img className='parnter-second-icon' src={youtube} alt="telegram" />
+                    <img className='parnter-second-img' src={elliot} alt="caesar" />
+                    <svg className='partner-second-bg-circle' width="210" height="210" viewBox="0 0 210 210" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="105" cy="105" r="20.8" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="34.6667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="48.5333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="62.4" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="76.2667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="90.1333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="104" stroke="white" stroke-width="2"/>
+                    </svg>
+                </div>
+
+                <div className="partner-second-desc">
+                    <h4>EllioTrades Crypto </h4>
+                    <p>Lorem ipsum dolor sit amet, consectetuer<br/> adipiscing elit, sed diam nonummy nibh<br/> euismod tincidunt ut laoreet </p>
+                </div>
+
             </div>
 
             <div className="partner-third">
+
+                <div className="partner-third-images">
+                    <img className='parnter-third-icon' src={twitter} alt="telegram" />
+                    <img className='parnter-third-img' src={travlado} alt="caesar" />
+                    <svg className='partner-third-bg-circle' width="210" height="210" viewBox="0 0 210 210" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="105" cy="105" r="20.8" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="34.6667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="48.5333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="62.4" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="76.2667" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="90.1333" stroke="white" stroke-width="2"/>
+                        <circle cx="105" cy="105" r="104" stroke="white" stroke-width="2"/>
+                    </svg>
+
+
+                </div>
+
+                <div className="partner-third-desc">
+                    <h4>Travladd Crypto</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetuer<br/> adipiscing elit, sed diam nonummy nibh<br/> euismod tincidunt ut laoreet </p>
+                </div>
 
             </div>
 
